0db15611f4616285034c6648eba65626bdfbd116
Justin Thomas <thomascjustin@gmail.com>
2018-12-06 18:03:29 -0500

    Add base for loop structure

--- scrape.py
+++ scrape.py
@@ -3,7 +3,14 @@ from bs4 import BeautifulSoup
 
 base_url="https://www.greatcourses.co"
 
-req = requests.get(base_url)
-content = req.content
 
-html_content = BeautifulSoup(c, "html.parser")
+
+# Specify the starting and stopping points for the scraping
+def page_range(start=1, stop=start+9):
+    full_url = base_url + str(start)
+    if start < stop:
+        for i in range(start, stop):
+            req = requests.get("{0}{1}".format(base_url, i))
+            content = req.content
+
+            html_content = BeautifulSoup(content, "html.parser")
