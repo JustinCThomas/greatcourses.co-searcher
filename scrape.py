22f285d5a59e9696b60bd8c1d2321a992e5c8e7b
Justin Thomas <thomascjustin@gmail.com>
2018-12-06 19:32:54 -0500

    Add function call to __name__

--- scrape.py
+++ scrape.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup
 
 base_url="https://www.greatcourses.co"
 
-
-page_start = input("Enter the starting page number: ")
-page_end = input("Enter the ending page number: ")
-
-try:
-    if page_start == '' || page_end == '':
-        page_range()
-    else:
-        page_range(int(page_start), int(page_end))
-except:
-    print("Something went wrong.\n Be sure to enter integers for the page numbers")
-
-
 # Specify the starting and stopping points for the scraping
-def page_range(start=1, stop=start+9):
+def page_range(start=1, stop=10):
     full_url = base_url + str(start)
     if start < stop:
         for i in range(start, stop):
             req = requests.get("{0}/page/{1}".format(base_url, i))
             content = req.content
-
+            print(req)
             html_content = BeautifulSoup(content, "html.parser")
+
+if __name__ == "__main__":
+    page_start = input("Enter the starting page number: ")
+    page_end = input("Enter the ending page number: ")
+
+    try:
+        if page_start == '' or page_end == '':
+            page_range()
+        else:
+            page_range(int(page_start), int(page_end))
+    except:
+        print("Something went wrong.\nBe sure to enter integers for the page numbers")
+    page_range()
