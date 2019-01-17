aff677401ab78a969bb2c8668bcce656fa96bc61
Justin Thomas <thomascjustin@gmail.com>
2019-01-17 16:57:07 -0500

    Add file export option

--- scrape.py
+++ scrape.py
@@ -5,8 +5,11 @@ from time import sleep
 base_url="https://www.greatcourses.co"
 
 # Specify the starting and stopping points for the scraping
-def scrape_page_range(start=1, stop=10):
+def scrape_page_range(start=1, stop=10, create_file=False):
     full_url = base_url + str(start)
+    if create_file:
+        file = open("course_information.txt", 'w')
+
     if start <= stop:
         for i in range(start, stop + 1):
             print("Now scraping page %s...\n" % i)
@@ -20,19 +23,26 @@ def scrape_page_range(start=1, stop=10):
                 if cards[i].find(class_="card-footer").find("b").get_text() == 'FREE':
                     free_courses.append(cards[i])
             for i in free_courses:
+                text_to_write = ""
                 print(i.find(class_="card-title").find("b").get_text())
+                title = i.find(class_="card-title").find("b").get_text()
 
                 language = i.find(class_="card-body").find_all("h6")[0].find("b").get_text()
                 print("Language: %s" % language)
 
-                language = i.find(class_="card-body").find_all("h6")[1].find("b").get_text()
-                print("Category: %s" % language)
+                category = i.find(class_="card-body").find_all("h6")[1].find("b").get_text()
+                print("Category: %s" % category)
 
                 gc_co_url = base_url + i.find(class_="card-footer").find("a")["href"]
                 url_split = gc_co_url.split("/")
                 course_number = url_split[4]
                 udemy_url = "{}/goto/{}".format(base_url, course_number)
                 print(udemy_url)
+
+
+                text_to_write = "{}\n{}\n{}\n{}\n\n".format(title, language, category, udemy_url)
+                if create_file:
+                    file.write(text_to_write)
 
                 print()
             print()
@@ -48,8 +58,13 @@ if __name__ == "__main__":
 
     scrape_all = input("Do you want to scrape all of the pages?\nThere are %s pages. (y for yes, anything else for no)" % last_page)
 
+    file_output_choice = input("Would you like a file with course information? (y for yes, anything else for no)")
+    file_output_choice = True if file_output_choice.lower() == 'y' else False
+
+
+
     if scrape_all.lower() == 'y':
-        scrape_page_range(1, last_page)
+        scrape_page_range(1, last_page, file_output_choice)
 
     page_start = input("Enter the starting page number: ")
     page_end = input("Enter the ending page number: ")
@@ -58,12 +73,12 @@ if __name__ == "__main__":
     try:
         if page_start == '' or page_end == '':
             print("Using the default page settings: 1-10.")
-            scrape_page_range()
+            scrape_page_range(create_file=file_output_choice)
         else:
             print("MANUAL")
             page_start = 1 if int(page_start) < 1 else page_start
             page_end = last_page if int(page_end) > last_page else page_end
-            scrape_page_range(int(page_start), int(page_end))
+            scrape_page_range(int(page_start), int(page_end), file_output_choice)
     except Exception as e:
         print()
         print("Something went wrong.\nBe sure to enter integers for the page numbers.")
