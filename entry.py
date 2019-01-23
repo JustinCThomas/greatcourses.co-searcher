65bf6ef07fe1e1eac5fe108413b32ce7558d03f9
Justin Thomas <thomascjustin@gmail.com>
2019-01-23 17:13:55 -0500

    Add base page search form

--- entry.py
+++ entry.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        page_start = request.form['page_start']
+        page_end = request.form['page_end']
+
     return render_template("home.html")
 
 if __name__== "__main__":
